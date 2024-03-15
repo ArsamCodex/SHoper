557ecacd3da7d75b2cbb08e8e4a315b9625ac69d
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-03-15 21:28:06 +0000

    Add project files.

---
+++ Data/ApplicationDbMyData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SHoper.Data
+{
+    public class ApplicationDbMyData :DbContext
+    {
+        public ApplicationDbMyData(DbContextOptions<ApplicationDbMyData> options)
+          : base(options)
+        {
+        }
+    }
+}
