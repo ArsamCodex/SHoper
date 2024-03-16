75ae0408a8b8c6ef668dfaef0e447dba53060741
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-03-16 00:38:39 +0000

    Admin Test

---
+++ Controller/RoleController.cs
@@ -0,0 +1,12 @@
+﻿namespace SHoper.Controller
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class RoleController :Controller
+    {
+        public string RoleName()
+		{
+			return "Armin";
+		}
+    }
+}
